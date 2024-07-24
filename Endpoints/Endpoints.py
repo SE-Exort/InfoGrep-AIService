84d0ba754fceac9558f1bf4525f4eebba9acf78a
Gary Li <gary.li1@uwaterloo.ca>
2024-07-23 20:51:42 -0400

    Add local wrapper

--- Endpoints/Endpoints.py
+++ Endpoints/Endpoints.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 
 from InfoGrep_BackendSDK import authentication_sdk, room_sdk
 
-from LLMWrapers import OpenAI
+from LLMWrapers import Local
 
 router = APIRouter(prefix='/api', tags=["api"]);
 
@@ -13,16 +13,16 @@ In the future, we should take the chatroom uuid, the cookie, and the message uui
 Or should we just have a webhook and generate a response when needed instead of exposing an endpoint.
 Either way, this service should send the message to the chatroom service."""
 @router.get('/system_response')
-async def get_system_Response(chatroom_uuid, message, cookie, openAIkey):
+async def get_system_Response(chatroom_uuid, message, cookie):
     #authenticate user
     #user must have a valid session cookie
     user = authentication_sdk.User(cookie)
     user_uuid = user.profile()['user_uuid'];
     room_sdk.get_userInRoom(chatroom_uuid=chatroom_uuid, cookie=cookie);
 
-    a = OpenAI.OpenAI(chatroom_uuid=chatroom_uuid, cookie=cookie);
-    a.set_openAIkey(openAIkey)
+    a = Local.Local(chatroom_uuid=chatroom_uuid, cookie=cookie);
     summarizedresponse = a.summarize(message);
 
     room_sdk.post_message(chatroom_uuid=chatroom_uuid,message=summarizedresponse, cookie='infogrep-chatbot-summary')
+    return summarizedresponse
 
