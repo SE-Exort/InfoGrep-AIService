0a09dee01fb22b92068b2537ad6d8471d7a89285
Gary Li <gary.li1@uwaterloo.ca>
2025-01-24 12:44:58 -0500

    add request.headers for tracing

--- Endpoints/Endpoints.py
+++ Endpoints/Endpoints.py
@@ -14,29 +14,29 @@ In the future, we should take the chatroom uuid, the cookie, and the message uui
 Or should we just have a webhook and generate a response when needed instead of exposing an endpoint.
 Either way, this service should send the message to the chatroom service."""
 @router.get('/system_response')
-async def get_system_Response(chatroom_uuid, message, cookie, model):
+async def get_system_Response(request: Request, chatroom_uuid, message, cookie, model):
     #authenticate user
     #user must have a valid session cookie
     user = authentication_sdk.User(cookie, {})
     user_uuid = user.profile()['user_uuid']
-    room_sdk.get_userInRoom(chatroom_uuid=chatroom_uuid, cookie=cookie, headers={})
+    room_sdk.get_userInRoom(chatroom_uuid=chatroom_uuid, cookie=cookie, headers=request.headers)
 
     wrapper = None
     if model == "ollama":
         wrapper = Ollama.Ollama(chatroom_uuid=chatroom_uuid, cookie=cookie)
     else:
         wrapper = OpenAI.OpenAI(chatroom_uuid=chatroom_uuid, cookie=cookie)
-    summarizedresponse = wrapper.summarize(message);
+    summarizedresponse = wrapper.summarize(message)
 
-    r = room_sdk.post_message(chatroom_uuid=chatroom_uuid,message=summarizedresponse, cookie='infogrep-chatbot-summary', headers={}, model=model)
+    r = room_sdk.post_message(chatroom_uuid=chatroom_uuid, message=summarizedresponse, cookie='infogrep-chatbot-summary', headers=request.headers, model=model)
     print(r.text)
     return summarizedresponse
 
 @router.get('/models')
-async def get_models(cookie):
+async def get_models(request: Request, cookie):
     #authenticate user
     #user must have a valid session cookie
-    user = authentication_sdk.User(cookie, {})
+    user = authentication_sdk.User(cookie, request.headers)
 
     x = requests.get('http://localhost:11434/api/tags')
     model_list = x.json()['models']
