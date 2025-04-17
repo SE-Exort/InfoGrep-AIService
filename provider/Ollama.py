752417e9d0a2089c6cfe6def7c621f1541aa18c3
TyroneHe-0926 <tyrone99926@gmail.com>
2025-04-17 00:38:46 -0400

    fix: add ollama embedding base url param

--- provider/Ollama.py
+++ provider/Ollama.py
@@ -7,7 +7,7 @@ from langchain_ollama.llms import OllamaLLM
 
 class Ollama(Provider):
     def embedding(self, embedding_model: str):
-        return OllamaEmbeddings(model=embedding_model)
+        return OllamaEmbeddings(model=embedding_model, base_url=ollama_service_host)
 
     def llm(self, chat_model: str):
         return OllamaLLM(model=chat_model, base_url=ollama_service_host)
